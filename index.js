2c315b10f7530a3fb181150009458e7783790a86
Christian <ChrisWcs@users.noreply.github.com>
2017-11-06 16:20:37 -0700

    Add code to index.js

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("hello");
+console.log(Redux);
+
+// Grab necessary ui from the DOM
+const inputField = document.getElementById('in');
+const addBtn = document.getElementById('btn');
+const listCon = document.getElementById('listCon');
+
+// ACTIONS
+
+const ADD_TODO = "ADD_TODO";
+
+// Action creators
+
+const createAddTodo = (text) => ({
+    type: ADD_TODO,
+    todo: text
+})
+
+// Initial State
+
+const initialState = () => ({
+    todos: []
+});
+
+// Reducer
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case ADD_TODO:
+            return {
+                todos: [ ...state.todos, action.todo]
+            };
+
+        default:
+            return state;
+    }
+}
+
+// create store and add listener
+
+const store = Redux.createStore(reducer);
+
+addBtn.onclick = () => {
+    store.dispatch(createAddTodo(inputField.value));
+}
+
